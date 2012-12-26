532eda3545841ff7aa1b91bf584b59195e2ce517
Amber Yust <amber.yust@gmail.com>
2012-12-25 16:41:08 -0800

    Movable player sprite

--- js/novavim.js
+++ js/novavim.js
@@ -4,8 +4,11 @@ Novavim = {};
 
   var PLANET_RADIUS = 100;
 
+  var ROTATION_SPEED = Math.PI / 60;
+  var MOVEMENT_SPEED = 3.0;
+
   // Must be called before anything else.
-  Novavim.init = function(selector) {
+  Novavim.init = function(selector) { // {{{
     // selector should be for a canvas object.
     var elem = $(selector).first();
     Novavim.view = elem.get(0).getContext("2d");
@@ -30,77 +33,160 @@ Novavim = {};
     // Add a test object
     Novavim.world.insert(Novavim.planets[0]);
 
-    Novavim.run();
-  };
+    Novavim.player = {
+      "x": 320,
+      "y": 134,
+      "angle": 0
+    };
 
-  Novavim.run = function() {
+    Novavim.keys = {
+      "up": false,
+      "down": false,
+      "left": false,
+      "right": false,
+    };
+
+    $("body").keydown(Novavim.keydown);
+    $("body").keyup(Novavim.keyup);
+
+    Novavim.run();
+  }; // }}}
+
+  Novavim.run = function() { // {{{
     Novavim.update();
     requestAnimationFrame(Novavim.run);
-    Novavim.draw();
-  }
+    Novavim.render();
+  } // }}}
 
-  Novavim.update = function() {
-  };
+  Novavim.update = function() { // {{{
+    // Rotation
+    if(Novavim.keys.left && Novavim.keys.right) {
+      // No rotation if both keys pressed.
+    } else if(Novavim.keys.left) {
+      Novavim.player.angle -= ROTATION_SPEED;
+      if(Novavim.player.angle < 0) {
+        Novavim.player.angle += 2*Math.PI;
+      }
+    } else if(Novavim.keys.right) {
+      Novavim.player.angle += ROTATION_SPEED;
+      if(Novavim.player.angle >= 2*Math.PI) {
+        Novavim.player.angle -= 2*Math.PI;
+      }
+    }
 
-  Novavim.draw = function() {
+    // Movement
+    if(Novavim.keys.up) {
+      Novavim.player.x += Math.sin(Novavim.player.angle) * MOVEMENT_SPEED;
+      Novavim.player.y -= Math.cos(Novavim.player.angle) * MOVEMENT_SPEED;
+    }
+  }; // }}}
+
+  Novavim.render = function() { // {{{
     Novavim.clear();
 
     $.each(Novavim.planets, function(idx, val) {
-      Novavim.draw_planet(val.x, val.y);
+      Novavim.draw.planet(val.x, val.y);
     });
 
-    Novavim.draw_ship(320, 139);
-  };
+    var player = Novavim.player;
+    Novavim.draw.ship(player.x, player.y, player.angle);
+  }; // }}}
 
-  // DRAWING FUNCTIONS
-  Novavim.clear = function(color) {
+  Novavim.keydown = function(e) { // {{{
+    switch(e.which) {
+      case 37: // left arrow
+        Novavim.keys.left = true;
+        break;
+      case 38: // up arrow
+        Novavim.keys.up = true;
+        break;
+      case 39: // right arrow
+        Novavim.keys.right = true;
+        break;
+      case 40: // down arrow
+        Novavim.keys.down = true;
+        break;
+      default:
+        // Pass through non-recognized keys
+        return
+    }
+    e.preventDefault();
+  } // }}}
+
+  Novavim.keyup = function(e) { // {{{
+    switch(e.which) {
+      case 37: // left arrow
+        Novavim.keys.left = false;
+        break;
+      case 38: // up arrow
+        Novavim.keys.up = false;
+        break;
+      case 39: // right arrow
+        Novavim.keys.right = false;
+        break;
+      case 40: // down arrow
+        Novavim.keys.down = false;
+        break;
+      default:
+        // Pass through non-recognized keys
+        return
+    }
+    e.preventDefault();
+  } // }}}
+
+  Novavim.clear = function(color) { // {{{
     color = color || "#000";
 
     Novavim.view.fillStyle = color;
     Novavim.view.fillRect(0, 0, Novavim.width, Novavim.height);
-  };
+  }; // }}}
 
-  // x,y is the center of the planet
-  Novavim.draw_planet = function(x, y, color) {
-    color = color || "#fff";
+  // DRAWING FUNCTIONS
+  Novavim.draw = { // {{{
+    // x,y is the center of the planet
+    planet: function(x, y, color) { // {{{
+      color = color || "#666";
 
-    Novavim.view.save();
-    Novavim.view.translate(x, y);
-    Novavim.view.strokeStyle = color;
-    Novavim.view.beginPath();
-    Novavim.view.arc(0, 0, PLANET_RADIUS, 0, 2*Math.PI, true);
-    Novavim.view.closePath();
-    Novavim.view.stroke();
-    Novavim.view.restore();
-  };
+      Novavim.view.save();
+      Novavim.view.translate(x, y);
+      Novavim.view.strokeStyle = color;
+      Novavim.view.beginPath();
+      Novavim.view.arc(0, 0, PLANET_RADIUS, 0, 2*Math.PI, true);
+      Novavim.view.closePath();
+      Novavim.view.stroke();
+      Novavim.view.restore();
+    }, // }}}
 
-  // x,y is the base of the ship
-  Novavim.draw_ship = function(x, y, angle, color) {
-    color = color || "#c2d";
-    angle = angle || 0;
+    // x,y is the base of the ship
+    ship: function(x, y, angle, color) { // {{{
+      color = color || "#c2d";
+      angle = angle || 0;
 
-    Novavim.view.save();
-    Novavim.view.translate(x, y);
-    Novavim.view.rotate(angle);
-    Novavim.view.strokeStyle = color;
-    Novavim.view.beginPath();
+      Novavim.view.save();
+      Novavim.view.translate(x, y);
+      Novavim.view.rotate(angle);
+      Novavim.view.strokeStyle = color;
+      Novavim.view.beginPath();
 
-    Novavim.view.moveTo(0, 0);
-    Novavim.view.lineTo(-6, -6);
-    Novavim.view.lineTo(-10, 1);
-    Novavim.view.lineTo(-8, -10);
-    Novavim.view.lineTo(-4, -8);
-    Novavim.view.lineTo(0, -20);
-    Novavim.view.lineTo(4, -8);
-    Novavim.view.lineTo(8, -10);
-    Novavim.view.lineTo(10, 1);
-    Novavim.view.lineTo(6, -6);
-    Novavim.view.lineTo(0, 0);
-    
-    Novavim.view.closePath();
-    Novavim.view.stroke();
-    Novavim.view.restore();
-  };
+      Novavim.view.moveTo(0, 5);
+      Novavim.view.lineTo(-6, -1);
+      Novavim.view.lineTo(-10, 6);
+      Novavim.view.lineTo(-8, -5);
+      Novavim.view.lineTo(-4, -3);
+      Novavim.view.lineTo(0, -15);
+      Novavim.view.lineTo(4, -3);
+      Novavim.view.lineTo(8, -5);
+      Novavim.view.lineTo(10, 6);
+      Novavim.view.lineTo(6, -1);
+      Novavim.view.lineTo(0, 5);
+      
+      Novavim.view.closePath();
+      Novavim.view.stroke();
+      Novavim.view.restore();
+    } // }}}
 
+  }; // }}}
 
 })();
+
+// vim:set foldmethod=marker et ts=2 sw=2 sts=2:
